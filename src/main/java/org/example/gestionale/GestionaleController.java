173253d570f635bd65ad5b452c016ceb1fe04154
Mattia Montini <mattiamontini2006@gmail.com>
2024-01-19 13:13:18 +0100

    Aggiunti alert e ToggleGroup

--- src/main/java/org/example/gestionale/GestionaleController.java
+++ src/main/java/org/example/gestionale/GestionaleController.java
@@ -2,14 +2,21 @@ package org.example.gestionale;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 public class GestionaleController {
     public javafx.scene.control.DatePicker DatePicker;
+    public RadioButton rbMaschio;
+    public RadioButton rbFemmina;
+    public ToggleGroup Sesso;
     @FXML
     private TextField txtNome;
     @FXML
     private TextField txtCognome;
+    
 
     @FXML
     void initialize() {
@@ -18,5 +25,14 @@ public class GestionaleController {
     }
 
     public void onButtonCreaDipendente(ActionEvent event) {
+        if (txtNome.getText().equals("") || txtCognome.getText().equals("") || DatePicker.getValue().equals("")){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Errore");
+            alert.setHeaderText("ERRORE D'INSERIMENTO");
+            alert.setContentText("Inserire tutti i campi richiesti.");
+            alert.showAndWait();
+        }
+
+
     }
 }
