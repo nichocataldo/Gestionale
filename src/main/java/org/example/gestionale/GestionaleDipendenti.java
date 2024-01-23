1d6f62b1e83ae98ef9c8ed69cf4b6104030f9ae1
Yu Dong Wu <yu.dong0502.education@gmail.com>
2024-01-23 17:26:56 +0100

    Finito la scrittura dei metodi "caricaDipendente" e "salvaDipendente" Aggiunte prime bozze di descrizioni dei metodi

--- src/main/java/org/example/gestionale/GestionaleDipendenti.java
+++ src/main/java/org/example/gestionale/GestionaleDipendenti.java
@@ -1,12 +1,11 @@
 package org.example.gestionale;
 
-import java.io.BufferedReader;
+import java.io.*;
 import java.util.Scanner;
 
 public class GestionaleDipendenti {
     private String nome;
     private String cognome;
-    private int gg, mm, anno;
     private String data;
     private String sesso;
 
@@ -15,25 +14,59 @@ public class GestionaleDipendenti {
      */
     public GestionaleDipendenti(){};
 
-    public void creaDipendente(String no, String co, String se, String da){
-        this.nome = no;
-        this.cognome = co;
-        this.sesso = se;
-        this.data = da;
+    /**
+     * Metodo che permette l'aggiunta di un dipendente
+     * @param nome
+     * @param cognome
+     * @param sesso
+     * @param data
+     */
+    public void creaDipendente(String nome, String cognome, String sesso, String data){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.sesso = sesso;
+        this.data = data;
     }
 
-    public void modificaDipendente(String no, String co, String se, String da){
-        this.nome = no;
-        this.cognome = co;
-        this.sesso = se;
-        this.data = da;
+    /**
+     * Metodo che permette la modifica di dati anagrafici di un dipendente, passando i dati aggiornati.
+     * @param nome
+     * @param cognome
+     * @param sesso
+     * @param data
+     */
+    public void modificaDipendente(String nome, String cognome, String sesso, String data){
+        this.nome = nome;
+        this.cognome = cognome;
+        this.sesso = sesso;
+        this.data = data;
     }
 
-    public void caricaDipedente(){
-
+    /**
+     * Metodo che permette il caricamento di dati dei dipendenti precedentemente salvati.
+     * @throws IOException
+     */
+    // Nota: Da ridiscutere (molto probabilmente il read lo si deve fare nel Controller)
+    public void caricaDipedenti() throws IOException {
+        FileReader file = new FileReader("dipendenti.cvs");
+        BufferedReader fileRead = new BufferedReader(file);
+        String riga;
+        String[] dati;
+        riga = fileRead.readLine();
+        dati = riga.split(";");
+        this.nome = dati[0];
+        this.cognome = dati[1];
+        this.sesso = dati[2];
+        this.data = dati[3];
     }
 
-    public void salvaDipendente(){
-
+    /**
+     * Metodo che permette di salvare i dati attuali in un file ".cvs" (*comma separated value*) in modo da poterli caricare alla prossima apertura senza dover ricompilare i dati.
+     * @throws IOException
+     */
+    public void salvaDipendenti() throws IOException {
+        FileWriter file = new FileWriter("dipendenti.cvs");
+        BufferedWriter fileWrite = new BufferedWriter(file);
+        fileWrite.write(this.nome + ";" + this.cognome + ";" + this.sesso + ";" + this.data);
     }
 }
