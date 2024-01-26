b5c997de02a23f69d19d3fc9dbe5e49255aea148
Yu Dong Wu <yu.dong0502.education@gmail.com>
2024-01-26 13:44:20 +0100

    - Classe "GestioneDipendenti" rinominata a "Dipendenti" - Sistemato metodi "caricaDipendenti" e "salvaDipendenti", ora verranno caricati all'apertura e salvati ad ogni modifica. - Aggiunto la possibilità di modificare i dati di un dipendente.

--- src/main/java/org/example/gestionale/GestionaleController.java
+++ src/main/java/org/example/gestionale/GestionaleController.java
@@ -2,15 +2,12 @@ package org.example.gestionale;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleGroup;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.control.*;
 import org.controlsfx.control.SearchableComboBox;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.util.ArrayList;
 
 public class GestionaleController {
     public javafx.scene.control.DatePicker DatePicker;
@@ -23,33 +20,88 @@ public class GestionaleController {
     private TextField txtNome;
     @FXML
     private TextField txtCognome;
-    private GestionaleDipendenti Gestionale;
+    @FXML
+    private TextField txtModificaNome, txtModificaCognome, txtModificaData;
+    @FXML
+    private RadioButton rbModificaMaschio, rbModificaFemmina;
+    @FXML
+    private Label sessoDipendente;
+    private int index = -1;
+    private Dipendenti Gestionale;
     private String sesso;
-
+    private ArrayList<Dipendenti> dipendenti = new ArrayList<>();
     @FXML
     void initialize() throws IOException {
         txtNome.setPromptText("Nome");
         txtCognome.setPromptText("Cognome");
         DatePicker.setPromptText("Data");
-        Gestionale = new GestionaleDipendenti();
-        ListaDipendenti.getItems().add("Mattia Montini");
+        Gestionale = new Dipendenti();
+        dipendenti.addAll(Dipendenti.caricaDipedenti());
+        for (int i = 0; i < dipendenti.size(); i++){
+            ListaDipendenti.getItems().add(dipendenti.get(i).getNome() + " " + dipendenti.get(i).getCognome()); // Aggiunge al CheckBox
+        }
+        System.out.println(dipendenti);
     }
     public void onButtonCreaDipendente(ActionEvent event) throws IOException {
-        if (txtNome.getText().equals("") || txtCognome.getText().equals("") || DatePicker.getValue().equals("")){
+        if (txtModificaNome.getText().isEmpty() || txtCognome.getText().isEmpty() || DatePicker.getValue() == null || (!rbFemmina.isSelected() && !rbMaschio.isSelected())){
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Errore");
             alert.setHeaderText("ERRORE D'INSERIMENTO");
             alert.setContentText("Inserire tutti i campi richiesti.");
             alert.showAndWait();
+        } else {
+            if(rbMaschio.isSelected())
+                sesso = "Maschio";
+            if(rbFemmina.isSelected())
+                sesso = "Femmina";
+            dipendenti.add(Dipendenti.creaDipendente(txtNome.getText(), txtCognome.getText(),sesso, String.valueOf(DatePicker.getValue())));
+            ListaDipendenti.getItems().add(dipendenti.get(dipendenti.size()-1).getNome() + " " + dipendenti.get(dipendenti.size()-1).getCognome());
+            Dipendenti.salvaDipendenti(dipendenti);
+            txtNome.clear();
+            txtCognome.clear();
+            rbMaschio.setSelected(false);
+            rbFemmina.setSelected(false);
         }
-        if(rbMaschio.isSelected())
-            sesso = "Maschio";
-        if(rbFemmina.isSelected())
-            sesso = "Femmina";
-        Gestionale.creaDipendente(txtNome.getText(), txtCognome.getText(),sesso, String.valueOf(DatePicker.getValue()));
-        Gestionale.salvaDipendenti();
     }
-    public void onButtonMostraDati(ActionEvent event) {
+    public void onButtonModificaDipendente(ActionEvent actionEvent) throws IOException {
+        Dipendenti dipendente = null;
+        if (index != -1) {
+            dipendente = dipendenti.get(index);
+        }
+        if (txtModificaNome.getText().isEmpty() || txtModificaCognome.getText().isEmpty() || txtModificaData.getText().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Errore");
+            alert.setHeaderText("ERRORE D'INSERIMENTO");
+            alert.setContentText("Inserire tutti i campi richiesti.");
+            alert.showAndWait();
+        } else {
+            if (rbModificaMaschio.isSelected())
+                sesso = "Maschio";
+            else if (rbModificaFemmina.isSelected())
+                sesso = "Femmina";
+            else
+                sesso = dipendente.getSesso();
+        }
+        dipendenti.get(index).modificaDipendente(txtModificaNome.getText(),txtModificaCognome.getText(),sesso,txtModificaData.getText());
+        Dipendenti.salvaDipendenti(dipendenti);
+        txtModificaNome.setText(dipendente.getNome());
+        txtModificaCognome.setText(dipendente.getCognome());
+        txtModificaData.setText(dipendente.getData());
+        sessoDipendente.setText(dipendente.getSesso());
     }
+    public void onListaDipendentiAction(ActionEvent actionEvent) {
+        index = ListaDipendenti.getSelectionModel().getSelectedIndex();
+        Dipendenti dipendente = dipendenti.get(index);
+        txtModificaNome.setText(dipendente.getNome());
+        txtModificaCognome.setText(dipendente.getCognome());
+        txtModificaData.setText(dipendente.getData());
+        sessoDipendente.setText(dipendente.getSesso());
+    }
+
+    void shutdown() throws IOException {
+        Dipendenti.salvaDipendenti(dipendenti);
+    }
+
+
 
 }
