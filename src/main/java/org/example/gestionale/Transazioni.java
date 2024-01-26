3e306242c8ee411977eefdd24fdc0608ced134ef
Mattia Montini <mattiamontini2006@gmail.com>
2024-01-26 22:32:17 +0100

    Inizio scrittura metodi per la classe Transazioni

--- src/main/java/org/example/gestionale/Transazioni.java
+++ src/main/java/org/example/gestionale/Transazioni.java
@@ -1,4 +1,37 @@
 package org.example.gestionale;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Transazioni {
+    private String importo;
+    public Transazioni(){}
+    static public Transazioni nuovaTransazione(String importo){
+        Transazioni transazioni = new Transazioni();
+        transazioni.importo = importo;
+        return transazioni;
+    }
+    static public ArrayList<Transazioni> caricaTransazioni() throws IOException {
+        ArrayList<Transazioni> transazioni = new ArrayList<>();
+        FileReader fr = new FileReader("src/main/java/Module/Transazioni.cvs");
+        BufferedReader br = new BufferedReader(fr);
+        String riga;
+        while ((riga = br.readLine()) != null) {
+            Transazioni transazione = new Transazioni();
+            System.out.println(riga);
+            transazioni.add(transazione);
+            System.out.println("Aggiunto " + transazione);
+            riga = br.readLine();
+        }
+        br.close();
+        fr.close();
+        return transazioni;
+    }
+    static public void salvaTransazioni(ArrayList<GestionaleFornitori> fornitori) throws IOException {
+        FileWriter file = new FileWriter("src/main/java/Module/Transazioni.cvs");
+        BufferedWriter bw = new BufferedWriter(file);
+
+        bw.close();
+        file.close();
+    }
 }
